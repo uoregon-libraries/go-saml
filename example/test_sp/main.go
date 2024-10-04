d3f11d831c8e724bbc0c37ddd70e6b7c1564726c
lsat12357 <lsato@uoregon.edu>
2024-10-04 11:29:16 -0700

    initial

---
+++ example/test_sp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+  "github.com/crewjam/saml/samlsp"
+  "github.com/tidwall/gjson"
+  "net/http"
+  "log"
+  "os"
+  "path/filepath"
+  "github.com/uoregon-libraries/go_saml/saml"
+)
+
+func main(){
+  e := echo.New()
+
+  var samlmw = saml.SamlMW{ Mware: saml.SamlSetup() }
+
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+  e.POST("/saml/acs", samlmw.SamlReturnHandler)
+  e.GET("/saml/metadata", samlmw.SamlMetadata)
+  g := e.Group("/greets")
+  g.Use(samlmw.SamlAuth)
+  g.Use(saml.CheckUser)
+
+  g.GET("/hello", hello)
+
+  e.Logger.Fatal(e.Start(":8080"))
+  }
+
+func hello(c echo.Context) error{
+  name := samlsp.AttributeFromContext(c.Request().Context(), "eduPersonPrincipalName")
+  return c.String(http.StatusOK, "hello: " + name)
+}
+
---
+++ saml_auth.go
@@ -0,0 +1,100 @@
+package saml
+
+import(
+  "context"
+  "crypto/rsa"
+  "crypto/tls"
+  "crypto/x509"
+  "net/http"
+  "net/url"
+  "path/filepath"
+  "github.com/crewjam/saml/samlsp"
+  "github.com/labstack/echo/v4"
+  "github.com/tidwall/gjson"
+  "fmt"
+  "log"
+  "os"
+)
+
+type SamlMW struct{
+  Mware *samlsp.Middleware
+}
+// redoes serveACS
+func (m SamlMW)SamlReturnHandler(c echo.Context) error{
+  err := c.Request().ParseForm()
+  if err != nil { m.Mware.OnError(c.Response(), c.Request(), err); return err }
+  possibleRequestIDs := []string{}
+  if m.Mware.ServiceProvider.AllowIDPInitiated {
+    possibleRequestIDs = append(possibleRequestIDs, "")
+  }
+  trackedRequests := m.Mware.RequestTracker.GetTrackedRequests(c.Request())
+  for _, tr := range trackedRequests {
+    possibleRequestIDs = append(possibleRequestIDs, tr.SAMLRequestID)
+  }
+  assertion, err := m.Mware.ServiceProvider.ParseResponse(c.Request(), possibleRequestIDs)
+  if err != nil { m.Mware.OnError(c.Response(), c.Request(), err); return err}
+  m.Mware.CreateSessionFromAssertion(c.Response(), c.Request(), assertion, m.Mware.ServiceProvider.DefaultRedirectURI)
+  return nil
+}
+
+func SamlSetup()(*samlsp.Middleware){
+  cert := os.Getenv("SAML_CERT")
+  key := os.Getenv("SAML_KEY")
+  idp_url := os.Getenv("SAML_IDP_METADATA_URL")
+
+  keyPair, err := tls.LoadX509KeyPair(cert, key)
+  if err != nil { log.Println(err) }
+  keyPair.Leaf, err = x509.ParseCertificate(keyPair.Certificate[0])
+  if err != nil { log.Println(err) }
+  idpMetadataURL, err := url.Parse(idp_url)
+  if err != nil { log.Println(err) }
+
+  // Initialize metadata of the IDP
+  idpMetadata, err := samlsp.FetchMetadata(context.Background(), http.DefaultClient, *idpMetadataURL)
+  if err != nil { log.Println(err) }
+  rootURL, err := url.Parse(base_url)
+  if err != nil { log.Println(err) }
+
+  samlSP, _ := samlsp.New(samlsp.Options{
+    URL: *rootURL,
+    IDPMetadata: idpMetadata,
+    Key: keyPair.PrivateKey.(*rsa.PrivateKey),
+    Certificate: keyPair.Leaf,
+  })
+  return samlSP
+}
+
+func (m SamlMW) SamlMetadata( c echo.Context) error{
+  m.Mware.ServeMetadata(c.Response(), c.Request())
+  return nil
+}
+
+func CheckUser(next echo.HandlerFunc) echo.HandlerFunc{
+  return func(c echo.Context) error{
+    email := samlsp.AttributeFromContext(c.Request().Context(), "eduPersonPrincipalName")
+    home_dir := os.Getenv("HOME_DIR")
+    json, err := os.ReadFile(filepath.Join(home_dir, "files/users.json"))
+    if err != nil { log.Println(err); return echo.NewHTTPError(400, err) }
+	query := fmt.Sprintf("users.#(email=\"%s\")", email)
+    value := gjson.GetBytes(json, query)
+    if value.Exists(){ return next(c) }else{ return c.String(http.StatusOK, "not auth") }
+    }
+}
+// redoes RequireAccount
+func (m SamlMW)SamlAuth(next echo.HandlerFunc) echo.HandlerFunc{
+  return func(c echo.Context) error{
+    session, err := m.Mware.Session.GetSession(c.Request())
+    if session != nil { 
+      r := c.Request().WithContext(samlsp.ContextWithSession(c.Request().Context(), session))
+      c.SetRequest(r)
+      return next(c)
+    }
+    if err == samlsp.ErrNoSession {
+      m.Mware.HandleStartAuthFlow(c.Response(), c.Request())
+      return nil
+    }
+    log.Println(err)
+    m.Mware.OnError(c.Response(), c.Request(), err)
+    return nil
+  }
+}
